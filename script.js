5be7538ddb36cba337408fcee87040c332f92a34
Ksenya Kiseleva <vvorobushekk7@gmail.com>
2025-02-15 13:29:45 +0500

    ready-made version

--- script.js
+++ script.js
@@ -1,89 +1,11 @@
-// Экземпляр Vue для автора
-const authorApp = Vue.createApp({
+import orders from './orders.js';
+import users from "./users";
+
+export default {
     data() {
         return {
-            author: {
-                name: "Александр Сергеевич Пушкин",
-                age: 37,
-            },
+            orders,
+            users
         };
-    },
-});
-
-authorApp.mount("#author-app");
-
-// Экземпляр Vue для книг
-const booksApp = Vue.createApp({
-    data() {
-        return {
-            books: [
-                { id: 1, title: "Евгений Онегин" },
-                { id: 2, title: "Дубровский" },
-                { id: 3, title: "Руслан и Людмила" },
-            ],
-        };
-    },
-});
-
-booksApp.mount("#books-app");
-
-// Экземпляр Vue с хуками жизненного цикла
-const hooksApp = Vue.createApp({
-    data() {
-        return { message: "Работа с хуками жизненного цикла" };
-    },
-    beforeCreate() {
-        console.log("beforeCreate: Vue экземпляр инициализируется.");
-    },
-    created() {
-        console.log("created: Данные и методы инициализированы.");
-    },
-    beforeMount() {
-        console.log("beforeMount: Готовимся к монтированию DOM.");
-    },
-    mounted() {
-        console.log("mounted: DOM смонтирован.");
-    },
-    beforeUpdate() {
-        console.log("beforeUpdate: Обновление данных.");
-    },
-    updated() {
-        console.log("updated: Данные обновлены в DOM.");
-    },
-});
-
-hooksApp.mount("#hooks-app");
-
-// Внешние данные
-const dataUser = {
-    name: "Иван Иванов",
-    age: 30,
+    }
 };
-
-const dataOrder = [
-    { id: 101 },
-    { id: 102 },
-    { id: 103 },
-];
-
-// Экземпляр Vue для внешнего объекта dataUser
-const userApp = Vue.createApp({
-    data() {
-        return {
-            user: dataUser,
-        };
-    },
-});
-
-userApp.mount("#external-data-user");
-
-// Экземпляр Vue для внешнего массива dataOrder
-const orderApp = Vue.createApp({
-    data() {
-        return {
-            orders: dataOrder,
-        };
-    },
-});
-
-orderApp.mount("#external-data-order");
---
+++ users.js
@@ -0,0 +1,6 @@
+let user = {
+    surname: "Зорина",
+    name: "Ирина",
+    login: "IrinaMpk",
+    email: "irina-mpk@gmail.com"
+}
