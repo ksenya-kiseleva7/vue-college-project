0b12ffbd10b2f398315da9e32f3571dc6a041839
Ksenya Kiseleva <vvorobushekk7@gmail.com>
2025-02-28 12:37:16 +0500

    ready-made version

---
+++ orders.js
@@ -0,0 +1,5 @@
+export const orders = [
+    { id: 1, name: "Книга", price: 500, quantity: 2 },
+    { id: 2, name: "Ручка", price: 100, quantity: 5 },
+    { id: 3, name: "Ноутбук", price: 30000, quantity: 1 }
+];
---
+++ user.js
@@ -0,0 +1,5 @@
+export const user = {
+    id: 1,
+    name: "Иван Иванов",
+    discount: 0.1 // Скидка 10%
+};
